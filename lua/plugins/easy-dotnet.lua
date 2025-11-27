172fd908a3ffe5ebecf9f9318b10666ec8f81e77
Mathias Hemmer <mathias.f.hemmer@gmail.com>
2025-11-26 21:58:02 -0300

    added easy-dotnet

---
+++ lua/plugins/easy-dotnet.lua
@@ -0,0 +1,12 @@
+return -- lazy.nvim
+{
+  "GustavEikaas/easy-dotnet.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", 'nvim-telescope/telescope.nvim', },
+  config = function()
+    require("easy-dotnet").setup({
+      debugger = {
+        bin_path = vim.fs.joinpath(vim.fn.stdpath("data"), "mason/bin/netcoredbg.cmd")
+      },
+    })
+  end
+}
